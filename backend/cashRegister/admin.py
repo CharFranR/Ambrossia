358286a578dc737cd2e4499472e8a18e42ff0f56
FranR <oscar.reyes@est.ulsa.edu.ni>
2025-11-19 01:13:39 -0600

    feat(cashRegister): Admin

--- backend/cashRegister/admin.py
+++ backend/cashRegister/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import cashRegister, cashMovement
 
-# Register your models here.
+@admin.register(cashRegister)
+class CashRegisterAdmin(admin.ModelAdmin):
+    list_display = ("id", "status", "cashierId", "opened_at", "closed_at")
+    search_fields = ("id", "cashierId")
+    list_filter = ("status", "cashierId")
+
+@admin.register(cashMovement)
+class cashMovementAdmin(admin.ModelAdmin):
+    list_display = ("cash_inflow", "cash_outflow", "amount", "method", "description",
+                    "created_at", "denominations", "cashierId")
+    search_fields = ("cashierId", "created_at")
+    list_filter = ("amount", "cashierId")
