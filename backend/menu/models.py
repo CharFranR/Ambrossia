0337243818861594de2113a2732c683dff23ab2c
CharFranR <oscar.reyes@est.ulsa.edu.ni>
2025-11-26 15:31:37 -0600

    feature(menu): custom CRUDS for products to save any product movement, add test

--- backend/menu/models.py
+++ backend/menu/models.py
@@ -1,21 +1,22 @@
 from django.db import models
 
-class productCategory(models.Model):
+# Categorias de productos
+class ProductCategory(models.Model):
     name = models.CharField(max_length = 20)
 
-class product(models.Model):
-    name = models.CharField(max_length = 200)
-    price =  models.IntegerField()
-    categoryId = models.ForeignKey(productCategory, on_delete= models.CASCADE)
-
-class cookbook(models.Model):
+# Modelos
+class CookBook(models.Model):
     name = models.CharField(max_length = 200)
     note = models.TextField(blank=True)
 
-class ingredient(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length = 200)
-    unit = models.CharField(max_length = 200)
+    price =  models.IntegerField()
+    cookbookId = models.ForeignKey(CookBook, on_delete= models.CASCADE)
+    categoryId = models.ForeignKey(ProductCategory, on_delete= models.CASCADE)
 
-class cookbookIngredient (models.Model):
-    recipe = models.ForeignKey(cookbook, on_delete=models.CASCADE)
-    ingredient = models.ForeignKey(ingredient, on_delete=models.CASCADE)
+class InventoryProduct(models.Model):
+    productId = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    lastUpdated = models.DateTimeField(auto_now=True)
+
